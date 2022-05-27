6d2dff2a839c8ee44dc5f9e14111fabd5a4c9f88
billkuo08 <billkuo08@gmail.com>
2022-05-27 12:38:03 -0700

    Update code.js

--- Repo/week 4/code.js
+++ Repo/week 4/code.js
@@ -18,28 +18,27 @@ grade = 95;
 // if the 0 <= grade < 50
 // print F
 
-if (grade < 100 && grade >= 90) {
-    console.log("A+")
-} else {
-    if (grade < 90 && grade >= 80)
-        console.log("B+")
-}
+if (grade < 100 && grade >= 90){
+    console.log("A+")}
     else {
-    if (grade < 80 && grade >= 60)
-        console.log("C+")
-}
-        else {
-    if (grade < 70 && grade >= 60)
-        console.log("D")
-}
-         else {
-    if (grade < 60 && grade >= 50)
-        console.log("D")
-}
+        if (grade < 90 && grade >= 80)
+            console.log("B+")
+            else {
+                if (grade < 80 && grade >= 60)
+                console.log("C+")
+                else {
+                    if (grade < 70 && grade >= 60)
+                        console.log("D")
+                    else {
+                        if (grade < 60 && grade >= 50)
+                        console.log("D")
+                    else { 
+                        console.log("F")
 
-console.log("F")
-
-
-
+                    }
+                }
+            }
+        }
+    }
 
 console.log("Bye!")
