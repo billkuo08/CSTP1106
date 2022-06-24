c092ae630cb878dfb9e99ae54bdb761d8640305e
billkuo08 <billkuo08@gmail.com>
2022-06-24 12:03:59 -0700

    lab 8 part 2 done

--- week8/code.js
+++ week8/code.js
@@ -52,41 +52,47 @@
 
 // $(document).ready(setup);
 
-function calculate_add(){
+function calculate_add() {
     console.log('calcute() got called');
     x = parseInt(jQuery('#x').val());
     y = parseInt(jQuery('#y').val());
-    jQuery('#result').html(x + y);
-    
+    jQuery('#result').html(`Result: ${x} + ${y} = ${x + y}`);
+    $('#history').append(`<span class='add_class'>${x} + ${y} = ${x + y} <br>`);
 
 
 }
-function calculate_subtract(){
+function calculate_subtract() {
     console.log('calcute() got called');
     x = parseInt(jQuery('#x').val());
     y = parseInt(jQuery('#y').val());
-    jQuery('#result').html(x - y);
+    jQuery('#result').html(`Result: ${x} - ${y} = ${x - y}`);
+    $('#history').append(`<span class='sub_class'>${x} - ${y} = ${x - y} <br>`);
+
 
 
 }
-function calculate_multiply(){
+function calculate_multiply() {
     console.log('calcute() got called');
     x = parseInt(jQuery('#x').val());
     y = parseInt(jQuery('#y').val());
-    jQuery('#result').html(x * y);
+    jQuery('#result').html(`Result: ${x} * ${y} = ${x * y}`);
+    $('#history').append(`<span class='multi_class'>${x} * ${y} = ${x * y} <br>`);
+
 
 
 }
-function calculate_divide(){
+function calculate_divide() {
     console.log('calcute() got called');
     x = parseInt(jQuery('#x').val());
     y = parseInt(jQuery('#y').val());
-    jQuery('#result').html(x / y);
+    jQuery('#result').html(`Result: ${x} / ${y} = ${x / y}`);
+    $('#history').append(`<span class='div_class'>${x} / ${y} = ${x / y} <br>`);
+
 
 
 }
 
-function setup(){
+function setup() {
     console.log('setup() got called');
     jQuery('#add').click(calculate_add);
     jQuery('#subtract').click(calculate_subtract);
