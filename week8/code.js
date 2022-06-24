8d5754ea5539dbef8c068792a61cc4d9c84cbc2f
billkuo08 <billkuo08@gmail.com>
2022-06-24 11:09:52 -0700

    lab 8 part 1 done

---
+++ week8/code.js
@@ -0,0 +1,141 @@
+// x = 5 // number
+
+// y = x + 40 // number
+
+// console.log(y); // ?
+
+
+// if(x > 10)
+// console.log('X is more than 10'); //?
+
+// else 
+//     console.log("X is less than or equal 10"); //?
+
+//     console.log('bye')
+
+
+// for(i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// console.log(i) // 11
+
+
+// i = 1
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// console.log(i) // 11
+
+// i = 1000
+// do {
+//     console.log(i);
+
+//     i++;
+// } while (i <= 10);
+
+// console.log(i);
+
+
+
+// function f1() {
+//     tmp = jQuery('#x').val();
+//     jQuery('#z').html(tmp);
+// }
+
+// function setup() {
+//     console.log('setup() got called');
+//     jQuery('#y').click(f1);
+
+// }
+
+// $(document).ready(setup);
+
+function calculate_add(){
+    console.log('calcute() got called');
+    x = parseInt(jQuery('#x').val());
+    y = parseInt(jQuery('#y').val());
+    jQuery('#result').html(x + y);
+    
+
+
+}
+function calculate_subtract(){
+    console.log('calcute() got called');
+    x = parseInt(jQuery('#x').val());
+    y = parseInt(jQuery('#y').val());
+    jQuery('#result').html(x - y);
+
+
+}
+function calculate_multiply(){
+    console.log('calcute() got called');
+    x = parseInt(jQuery('#x').val());
+    y = parseInt(jQuery('#y').val());
+    jQuery('#result').html(x * y);
+
+
+}
+function calculate_divide(){
+    console.log('calcute() got called');
+    x = parseInt(jQuery('#x').val());
+    y = parseInt(jQuery('#y').val());
+    jQuery('#result').html(x / y);
+
+
+}
+
+function setup(){
+    console.log('setup() got called');
+    jQuery('#add').click(calculate_add);
+    jQuery('#subtract').click(calculate_subtract);
+    jQuery('#multiply').click(calculate_multiply);
+    jQuery('#divide').click(calculate_divide);
+
+
+
+}
+
+jQuery(document).ready(setup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
