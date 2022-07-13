3d61761d1b0ccfb40fb3b79870a21d4f6c4135ad
billkuo08 <billkuo08@gmail.com>
2022-07-12 22:20:12 -0700

    assignment 2

--- Aissignments 2/code.js
+++ Aissignments 2/code.js
@@ -3,27 +3,25 @@
 function process_response(data) {
     const moviesArr = [];
     moviesArr.push(data);
-    console.log(moviesArr)
-    
-    
-    for (let movies of moviesArr) {
-        console.log(movies);
-        
-        
-    }
+    let infos = '';
 
+    for (let moviesData of moviesArr) {
+        for (let movies of moviesData.results) {
+            console.log(movies.original_title);
+            infos = `<p>${movies.original_title}</p>\n${movies.overview}`
 
+            $('#result').append(infos);
 
-    // $('#result').html();
-    // $('#result2').html();
-    // $('#result3').html(`<img id='theImg' src=https://image.tmdb.org/t/p/w500/${data.poster_path} />`);
-    // // https://image.tmdb.org/t/p/w500//pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg
-
+            // $('#result2').html(`<p>${movies.original_title}</p>\n${movies.overview}`);
+            // $('#result3').html(`<img id='theImg' src=https://image.tmdb.org/t/p/w500/${data.poster_path} />`);
+            // // https://image.tmdb.org/t/p/w500//pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg 
+        }
+    } 
 
 }
 
 function init_ajax() {
-    movie_name = $('#movie_name_input').val();
+    movie_name = $('#movie_name_input').val().toLowerCase();
     $.ajax({
         url: `https://api.themoviedb.org/3/search/movie?api_key=282b574a1934c914c2e6ff9803175c12&query=${movie_name}`,
         type: 'GET',
