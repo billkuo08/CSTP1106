5cc79420721c8a724e96fe57b683b6f937046fa7
billkuo08 <billkuo08@gmail.com>
2022-07-17 01:11:10 -0700

    a2 minor eidtings

--- Aissignments 2/code.js
+++ Aissignments 2/code.js
@@ -11,9 +11,9 @@ const process_response = (data) => {
         <img src="https://image.tmdb.org/t/p/w500/${data.results[movies].poster_path}"></img>`;
         $("#results").append(`${infos}<br>`);
         $("#results").append(`<button id="bigImg${movies}">Show big image</button><br><br>`);
-        backdropsArr.push(`<img src="https://image.tmdb.org/t/p/original/${data.results[movies].backdrop_path}" width=50%>`)
+        backdropsArr.push(`<img src="https://image.tmdb.org/t/p/original${data.results[movies].backdrop_path}"width="50%">`)
     }
-    
+
     for (let i = 0; i < backdropsArr.length; i++) {
         let backImg = `#bigImg${i}`;
 
