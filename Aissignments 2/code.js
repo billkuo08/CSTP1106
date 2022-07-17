6cf054fda4cb36262678fb4a0a486e7cab2b8e34
billkuo08 <billkuo08@gmail.com>
2022-07-16 21:34:23 -0700

    a2

--- Aissignments 2/code.js
+++ Aissignments 2/code.js
@@ -9,13 +9,14 @@ function process_response(data) {
     for (let movies in data.results) {
         infos = `<p>${data.results[movies].original_title}</p>\n<p>${data.results[movies].overview}</p>
         <img src="https://image.tmdb.org/t/p/w500/${data.results[movies].poster_path}"></img>`;
-        $("#results").append(`${infos}<br><button id="Alt${movies}"> Show alternative image </button><br><br>`);
-        backdropsArr.push(`<img src="https://image.tmdb.org/t/p/w500${data.results[movies].backdrop_path}">`)
+        $("#results").append(`${infos}<br>`);
+        $("#results").append(`<button id="alt${movies}"> Show alternative image </button><br><br>`);
+        backdropsArr.push(`<img src="https://image.tmdb.org/t/p/original/${data.results[movies].backdrop_path} width=100%">`)
     }
 
     for (i = 0; i < backdropsArr.length; i++) {
-        // let altImges = `#A${bigImges}`;
-        $(`#Alt${i}`).click(() => {
+        let altImges = `#alt${i}`;
+        $(altImges).click(() => {
             $("#right_div").empty().append(backdropsArr[i])
         });
     }
@@ -26,7 +27,7 @@ function process_response(data) {
 function init_ajax() {
     let movie_name = $("#movie_name_input").val();
     $.ajax({
-        url: `https://api.themoviedb.org/3/search/movie?api_key=282b574a1934c914c2e6ff9803175c12&query=${movie_name}`,
+        url: `https://api.themoviedb.org/3/search/movie?api_key=282b574a1934c914c2e6ff9803175c12&language=en-US&query=${movie_name}&page=1&include_adult=false`,
         type: 'GET',
         success: process_response
     })
