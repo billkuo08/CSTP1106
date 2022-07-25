1a57f835b663044b37bd0b966fc3c5e6eb771ae3
billkuo08 <billkuo08@gmail.com>
2022-07-24 17:01:38 -0700

    prev/next button issues

--- Assignments 3/code.js
+++ Assignments 3/code.js
@@ -1,20 +1,9 @@
-let infos = "";
-currentPage = 1;
+infos = "";
 data = "";
 pageNumber = 1;
 pageSize = "";
 
-const initajax = () => {
-    var name = $("#moviesname").val();
-    $.ajax({
-        url: `https://api.themoviedb.org/3/search/movie?api_key=282b574a1934c914c2e6ff9803175c12&query=${name}`,
-        type: "GET",
-        success: process_response
-    });
-}
-
-const process_response = (x) => {
-
+function process_response(x) {
     data = x
     pageNumber = 1
     display();
@@ -22,15 +11,30 @@ const process_response = (x) => {
 
 }
 
+function initajax() {
+    let name = $("#moviesname").val();
+    $.ajax({
+        url: `https://api.themoviedb.org/3/search/movie?api_key=282b574a1934c914c2e6ff9803175c12&query=${name}`,
+        type: "GET",
+        success: process_response
+    });
+}
 
-const display = () => {
+function backDropImg() {
+
+    bigImg = $(this).attr("id");
+    $("#rightdiv").html(`<img src="https://image.tmdb.org/t/p/original${bigImg}"width="50%">`)
+
+}
+
+
+function display() {
     $("#results").empty();
-    $("#rightdiv").empty();
 
     pageSize = Number(pageSize);
     firstIndex = pageSize * (pageNumber - 1);
     lastIndex = pageSize * (pageNumber - 1) + (pageSize - 1)
-    for (i = firstIndex; i <= lastIndex; i++) {
+    for (let i = firstIndex; i <= lastIndex; i++) {
         infos = `<p>${data.results[i].original_title}</p>\n<p>${data.results[i].overview}</p>
         <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"height="300px">`;
         $("#results").append(`${infos}<br>`);
@@ -40,43 +44,33 @@ const display = () => {
 
 }
 
-function paginateMenu() {
-    $("#button").empty();
-
-    for (i = 1; i <= Math.ceil(data.results.length / pageSize); i++) {
-        console.log(i)
-        $("#first_page").html(`<button class="first" id=${1}>First</button>`)
-        $("#button").append(`<button class ="display" id="${i}">${i}</button>`)
-        $("#last_page").html(`<button class="last" id=${Math.ceil(data.results.length / pageSize)}>Last</button>`)
-        
-    }
-
-}
-
-function backDropImg() {
- 
-    w = $(this).attr("id");
-    $("#rightdiv").html(`<img src="https://image.tmdb.org/t/p/original${w}"width="50%">`)
-
-}
-
-
 function changePageNum() {
     pageNumber = $(this).attr("id");
     pageNumber = Number(pageNumber);
-    display()
     console.log(pageNumber)
-    if (pageNumber > 1 || pageNumber === 7) {
-        $("#prev").html(`<button class="previous" id="${pageNumber - 1}">Prev.</button>`)
-        $("#next").html(`<button class="next" id="${pageNumber + 1}">Next</button>`)
+    if (pageNumber > 1) {
+        $("#prev").html(`<button class="previous" id="${- 1}">Prev.</button>`)
+        $("#next").html(`<button class="next" id="${j + 1}">Next</button>`)
+
+    }
+    display()
+
+}
+
+function paginateMenu() {
+    $("#button").empty();
+
+    for (let j = 1; j <= Math.ceil(data.results.length / pageSize); j++) {
+        $("#first_page").html(`<button class="first" id=${1}>First</button>`)
+        $("#button").append(`<button class ="display" id="${j}">${j}</button>`)
+        $("#last_page").html(`<button class="last" id=${Math.ceil(data.results.length / pageSize)}>Last</button>`)
+
     }
 
 }
 
 
-
-
-const setup = () => {
+function setup() {
     $("#getResults").click(initajax);
     $("#moviesname").keypress(function (event) {
         if (event.keyCode === 13) {
